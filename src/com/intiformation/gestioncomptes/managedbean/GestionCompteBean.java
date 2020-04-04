2106e44f82a32cc6fc734dcea1768c5401d9b851
PieDid <pierrickflaux@sfr.fr>
2020-04-04 19:48:04 +0200

    Pierrick :: 4 : Modification de l'authentification , ajout de primefaces, et de bootfaces

---
+++ src/com/intiformation/gestioncomptes/managedbean/GestionCompteBean.java
@@ -0,0 +1,15 @@
+package com.intiformation.gestioncomptes.managedbean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean( name="gestionCompte")
+@SessionScoped
+public class GestionCompteBean implements Serializable{
+
+	
+	
+	
+}
